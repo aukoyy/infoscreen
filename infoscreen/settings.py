087131505f19efe0e2ec75c34677272b74abca46
auk <oyvind.auk@gmail.com>
2017-08-17 14:50:52 +0200

    sfgsda

--- infoscreen/settings.py
+++ infoscreen/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 STATIC_URL = '/static/'
 
 #
