2c28e2adfff96eb9182c5827c4cd04ed7e99a156
auk <oyvind.auk@gmail.com>
2017-08-17 15:46:44 +0200

    nohost

--- infoscreen/settings.py
+++ infoscreen/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'infoscreen_database',
         'USER': 'infouser',
         'PASSWORD': 'testoyvind123',
-        'HOST': 'aukinfo',#'localhost',
+        'HOST': '',#'localhost',
         'PORT': '5432',
     }
 }
